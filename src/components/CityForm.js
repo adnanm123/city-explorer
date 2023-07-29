00a65aaea94a167567d353d905cdd0161563dbca
Adnan Mohamud <adnanmohamud6@gmail.com>
2023-07-28 19:27:47 -0700

    Polished up website

---
+++ src/components/CityForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class CityForm extends React.Component {
+  render() {
+    return (
+      <form onSubmit={this.props.handleLocationSubmit}>
+        <label>
+          Search for a City:
+          <input name="city" onChange={this.props.changeCityInput} />
+        </label>
+        <button type="submit">Explore!</button>
+      </form>
+    );
+  }
+}
+
+export default CityForm;
