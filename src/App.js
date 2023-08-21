4d0279f399699093a10841974f3032370913273a
Adnan Mohamud <adnanmohamud6@gmail.com>
2023-08-20 21:51:45 -0700

    completed lab 10

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends React.Component {
       let lat = cityData.data[0].lat;
       let lon = cityData.data[0].lon;
       let cityDisplayName = cityData.data[0].display_name;
-      
+
       this.setState(
         {
           cityData: cityData.data[0],
@@ -43,67 +43,67 @@ class App extends React.Component {
           cityDisplayName: cityDisplayName,
           success: true,
         },
-        
+
         () => {
           this.displayWeather(this.state.lat, this.state.lon);
           this.displayMovie(this.state.cityDisplayName);
         }
-        );
-      } catch (error) {
-        console.log("This is an error");
-        this.setState({
-          error: true,
-          success: false,
-          errorMessage: `An error occurred: ${
-            error.response ? error.response.status : error.message
-          }`,
-        });
-      }
-    };
-    
-    displayWeather = async (lat, lon) => {
-      const weatherData = `${process.env.REACT_APP_SERVER_URL}/weather?searchQuery=${this.state.cityName}&lat=${lat}&lon=${lon}`;
-      try {
-        const weather = await axios.get(weatherData);
-        this.setState({ weather: weather.data });
-      } catch (error) {
-        console.log(
-          "This is an error inside display weather function",
-          error.response.data
-          );
-          this.setState({ weatherError: error.response.data });
-        }
-      };
-      
-      displayMovie = async (cityDisplayName) => {
-        let cityName = cityDisplayName.split(",")[0];
-        const movieData = `${process.env.REACT_APP_SERVER_URL}/movies?location=${cityName}`;
-        try {
-          const movie = await axios.get(movieData);
-          this.setState({ movie: movie.data });
-        } catch (error) {
-          console.log(
-            "This is an error inside display movie function",
-            error.response.data
-            );
-            this.setState({ movieError: error.response.data });
-          }
-        };
-        
-        changeCityInput = (e) => {
-          this.setState({
-            cityName: e.target.value,
-          });
-        };
-        
-        render() {
-          return (
-            <main>
+      );
+    } catch (error) {
+      console.log("This is an error");
+      this.setState({
+        error: true,
+        success: false,
+        errorMessage: `An error occurred: ${
+          error.response ? error.response.status : error.message
+        }`,
+      });
+    }
+  };
+
+  displayWeather = async (lat, lon) => {
+    const weatherData = `${process.env.REACT_APP_SERVER_URL}/weather?searchQuery=${this.state.cityName}&lat=${lat}&lon=${lon}`;
+    try {
+      const weather = await axios.get(weatherData);
+      this.setState({ weather: weather.data });
+    } catch (error) {
+      console.log(
+        "This is an error inside display weather function",
+        error.response.data
+      );
+      this.setState({ weatherError: error.response.data });
+    }
+  };
+
+  displayMovie = async (cityDisplayName) => {
+    let cityName = cityDisplayName.split(",")[0];
+    const movieData = `${process.env.REACT_APP_SERVER_URL}/movies?location=${cityName}`;
+    try {
+      const movie = await axios.get(movieData);
+      this.setState({ movie: movie.data });
+    } catch (error) {
+      console.log(
+        "This is an error inside display movie function",
+        error.response.data
+      );
+      this.setState({ movieError: error.response.data });
+    }
+  };
+
+  changeCityInput = (e) => {
+    this.setState({
+      cityName: e.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <main>
         <h1 className="title">City Explorer</h1>
         <CityForm
           handleLocationSubmit={this.handleLocationSubmit}
           changeCityInput={this.changeCityInput}
-          />
+        />
         {this.state.error ? (
           <Alert variant="danger">{this.state.errorMessage}</Alert>
         ) : null}
